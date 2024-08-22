c5fae2d273192d1e4e8a973bfe418ba5b6174ba4
Sharvil Limaye <lim.sharvil@gmail.com>
2024-08-22 07:13:19 -0400

    Add unit tests with Python and started library specific files

---
+++ src/lightningxml.h
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2024 Sharvil Limaye
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `LICENSE.md` for details.
+ */
+
+#ifndef LIGHTNINGXML_H
+#define LIGHTNINGXML_H
+
+#define LIGHTNINGXML_VERSION "0.1.0"
+
+
+
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,18 +1,27 @@
+/**
+ * Copyright (c) 2024 Sharvil Limaye
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `LICENSE.md` for details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "log.h"
+#include "lightningxml.h"
 
 void printLogo();
 
 int main(int argc, char *argv[]) {
-    printLogo();
-
     // Determine whether to run based on args or request for args
     if (argc > 1) {
-        
+        for (int i = 1; argv[i] != NULL; i++) {
+            printf("%s\n", argv[i]);
+        }
+    } else {
+        return EXIT_FAILURE;
     }
-
     
     return EXIT_SUCCESS;
 }
@@ -20,7 +29,6 @@ int main(int argc, char *argv[]) {
 void printLogo() {
     const char *yellow = "\033[1;93m";
     const char *reset = "\033[0m"; // Reset color
-
     printf("%s================================================================================================\n", yellow);
     printf("================================================================================================\n");
     printf(" __                   __      __                                __   __           __        \n");
